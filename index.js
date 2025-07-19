23128ce410b6e44425a061facde247227f0ce0cb
Tonmoy Das <64586396+indiantonmoy@users.noreply.github.com>
2025-07-19 20:02:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const  http=require("http");
+
+const server=http.createServer((req,res)=>{
+res.statusCode=200;
+res.setHeader('Content-Type','text/html');
+res.end("<h1>Hello World!</h1>");
+});
+
+server.listen(8080,'127.0.0.1');
